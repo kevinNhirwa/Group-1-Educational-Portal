0490082a0df1fe71576d4d1f5ace2d720e116180
hirwaNkevin <hirwankevin@gmail.com>
2023-04-27 19:05:13 +0200

    add course controller, change title in layout

---
+++ LeapStageWeb/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace LeapStageWeb.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string userName, int ID = 000)
+        {
+            ViewData["userName"] = userName;
+            ViewData["ID"] = ID;
+
+            return View();
+        }
+    }
+}
