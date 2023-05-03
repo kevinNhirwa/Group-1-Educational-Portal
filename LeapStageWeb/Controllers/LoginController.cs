d926a8975b085d1d54929a4c886495ce3c0286dd
hirwaNkevin <hirwankevin@gmail.com>
2023-05-03 11:23:31 +0200

    change return page on login

--- LeapStageWeb/Controllers/LoginController.cs
+++ LeapStageWeb/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using LeapStageWeb.Models;
@@ -9,9 +11,38 @@ namespace LeapStageWeb.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index([Bind("Id,fullName,email,phone,password")] Parent parent)
+        public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Login([Bind("email,password")] Parent parent)
+        {
+
+            using (SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=LeapStageDB;Integrated Security=True"))
+            {
+                using (SqlCommand cmd = new SqlCommand("selectParent", con))
+                {
+                        con.Open();
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@email", parent.email);
+                        
+                        cmd.Parameters.AddWithValue("@password", parent.password);
+                        cmd.ExecuteNonQuery();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+
+                        //List<Parent> parent = cmd.DataReader()
+
+                    }
+                }
+            }
+
+            return View("../Courses/Index");
+        }
+
+
     }
 }
